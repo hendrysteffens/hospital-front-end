c8ff0e5e78fc035e3c9e5bdf56a04600fe4ba0b2
csvinhal <cristiano.vinhal10@gmail.com>
2017-08-26 13:44:06 -0300

    rota home criada

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,22 @@
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
+(function () {
+  angular.module('starter', ['ionic', 'starter.controllers', 'starter.directives'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+      $stateProvider.state('home', {
+        url: "/",
+        templateUrl: "../templates/home/home.html"
+      });
+
+      $urlRouterProvider.otherwise('/');
+    }])
+    .run(function ($ionicPlatform) {
+      $ionicPlatform.ready(function () {
+        if (window.StatusBar) {
+          StatusBar.styleDefault();
+        }
+      });
+    })
+})();
